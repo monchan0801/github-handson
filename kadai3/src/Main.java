92baae8030fc55b1d6e03ee8c62c2e4388aa3972
monchan0801 <140710805+monchan0801@users.noreply.github.com>
2023-11-09 15:57:38 +0900

    Add files via upload

---
+++ kadai3/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> shusseki_List = new ArrayList<>();
+
+        shusseki_List.add("田中");
+        shusseki_List.add("山田");
+        shusseki_List.add("鈴木");
+
+        System.out.println(shusseki_List.toString());
+    }
+}
